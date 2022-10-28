2078813ca9bd1b467243e4aa61d2d6c17572340f
gregs15 <gregoriopontim@hotmail.com>
2022-10-28 14:24:28 -0300

    third commit

--- Lanches/Models/Categoria.cs
+++ Lanches/Models/Categoria.cs
@@ -1,12 +1,24 @@
-﻿namespace Lanches.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lanches.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
 
+        [StringLength(100,ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [Display(Name ="Nome")]
         public string CategoriaNome { get; set; }
 
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage = "Informe a descricao da categoria")]
+        [Display(Name = "Nome")]
         public string Descricao { get; set; }
+
 
         public List<Lanche> Lanches { get; set; }
     }
--- Lanches/Startup.cs
+++ Lanches/Startup.cs
@@ -16,7 +16,7 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllersWithViews();
-        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LancheCs")));
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LanchesCs")));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
